36cd4b1b2824ca52f81a3633120d3728fcb36837
Gyusun Yeom <omniavinco@gmail.com>
2016-12-06 22:10:36 +0900

    seperate data from code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MdDialog } from '@angular/material';
 import * as _ from 'lodash';
 import { Location } from './location';
 import { LocationInfoComponent } from './location-info/location-info.component';
+import * as fetch from 'isomorphic-fetch';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Map';
   lat = 37.5650168;
   lng = 126.849121;
   selected_locations: Location[] = [];
-  all_locations: Location[] = [{
-    label: "TILT",
-    lat: 37.5588297,
-    lng: 126.93527859999995,
-    tag: ["술", "칵테일", "위스키", "조용", "10000원대"],
-    desc: `2층, 바
-https://twitter.com/bartilt`
-  }];
+  all_locations: Location[] = [];
   locations: Location[] = [];
 
   constructor(private _dialog: MdDialog) {}
+
+  ngOnInit() {
+    fetch('assets/locations.json')
+      .then(resp => resp.json())
+      .then(locations => this.all_locations = locations);
+  }
 
   filterLocations(filter: string) {
     const tags = _.map(filter.split(","), t => t.trim());
